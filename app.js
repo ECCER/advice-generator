ee6763422ef501e3fe8dbe48c988fe7cf5fef7bb
Eduardo Calli <educalli65@gmail.com>
2022-04-01 09:21:13 -0500

    added readme and screenshot

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const getQuote = async() => {
-  try {
-    const resp = await fetch('https://api.adviceslip.com/advice');
-    const data = await resp.json();
-    const slip = data.slip;
-    const {id, advice} = slip
-    
-    const numberDom = document.querySelector('span');
-    const adviceDom = document.querySelector('#quote');
-
-    numberDom.textContent = id;
-    adviceDom.textContent = `"${advice}"`;
-  } catch (error) {
-    console.warn(error);
-  }
-}
-
-getQuote()
 
 const boton = document.querySelector(".dice");
-boton.addEventListener("click", () => location.reload());
+
+boton.addEventListener("click", async() => {
+    try {
+      const resp = await fetch('https://api.adviceslip.com/advice');
+      const data = await resp.json();
+      const slip = data.slip;
+      const {id, advice} = slip
+      
+      const numberDom = document.querySelector('span');
+      const adviceDom = document.querySelector('#quote');
+  
+      numberDom.textContent = `#${id}`;
+      adviceDom.textContent = `"${advice}"`;
+    } catch (error) {
+      console.warn(error);
+    }
+});
